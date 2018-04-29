f26341c5dbe32ca68ce0b035261729abb1091642
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-04-29 17:10:30 -0400

    Salad Object created

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+// 
+// SCRIPTS FOR SALAD CLUB
+// 
+
+
+
+
+
+// IMAGE DISPLAY
+
+// for salad item,  retrieve item.imageNumber to get number of times to clone image.
+// position each iteration absolute, 
+// top: random number between top of salad and bottom of salad
+// left: random number between left of salad and right of salad.
+
+// display dressing inside dressing container
+
+
+// salad base selector toggles between image of kale, spring greens, soba noodles positioned inside flex container on bowl.
