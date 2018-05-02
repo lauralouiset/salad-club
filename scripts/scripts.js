b8b837e876f95240645a29c31c8d32f2e748088a
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-05-01 22:07:09 -0400

    CSS Styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@
 // 
 
 
-
+// Mix button -- on click, generates random numbers to select items from the salad object, displays them in the display boxes. displays nutrition information in nutrition table.
 
 
 // IMAGE DISPLAY
@@ -12,8 +12,51 @@
 // position each iteration absolute, 
 // top: random number between top of salad and bottom of salad
 // left: random number between left of salad and right of salad.
+// transform: rotate(Xdeg) - rotates image a random number between 180deg and -180deg.
 
 // display dressing inside dressing container
 
 
 // salad base selector toggles between image of kale, spring greens, soba noodles positioned inside flex container on bowl.
+
+
+//checkboxes superimposed overtop of salad items to select which items get remixed
+
+// FUNCTIONS
+
+// retrives random item from an array
+const getRandomItem = (array) => {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// DOCUMENT READY
+$(function () {
+
+    // SELECT SALAD BASE
+
+    $('base-btn').on('click', function(e){
+      e.preventDefault();
+      $('.salad-base').removeClass('visible');
+      $(this).toggleClass(visible);
+
+
+    });
+
+
+    // GENERATE SALAD BUTTON
+    $('.generate').on('click', function(e){
+      
+
+      
+    });
+
+
+
+
+
+
+
+
+
+});
